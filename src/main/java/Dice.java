4faf955ed91ce18a63cc0601bf8eecf54b377d2d
Mads Brøndum <llomul@gmail.com>
2018-11-02 14:02:45 +0100

    Game+Dice klasse
    Oprettet Game klasse + Dice klasse.

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,2 +1,16 @@
 public class Dice {
+
+    private int maxFaceValue;
+
+    public Dice(int maxFaceValue){
+        this.maxFaceValue = maxFaceValue;
+
+    }
+    public int roll(){
+        double d = Math.random()*maxFaceValue+1;
+        int faceValue = (int)d;
+        return faceValue;
+
+    }
+
 }
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,2 +1,30 @@
 public class Game {
+
+    public Dice dice1;
+    public Dice dice2;
+    public Player[] players;
+    public UIManager manager;
+
+    public Game(int numberOfPlayers, int diceOneMax, int diceTwoMax, UIManagaer UI1){
+        players = new Players[numberOfPlayers];
+        for(int i = 0; i < numberOfPlayers; i++){
+            players[i] = new Player();
+
+        }
+        dice1 = new Dice(diceOneMax);
+        dice2 = new Dice(diceTwoMax);
+        manager = UI1;
+
+    }
+    public void takeTurn(int playerID){
+
+        int roll1 = dice1.roll();
+        int roll2 = dice2.roll();
+        DiceRoll myRoll = new DiceRoll(roll1, roll2);
+        Player player = players[playerID];
+        player.moveForward(myRoll.getValue());
+        manager.updateUI();
+
+    }
+
 }
