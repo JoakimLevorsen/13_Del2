95ecd9f08ca05958589587d4293a9e3f10d43a67
Cecilie Drejer <ceciliek.drejer@gmail.com>
2018-11-02 13:06:24 +0100

    Added methods in Player and Account classes

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,16 @@
 public class Player {
+    public final int startBalance = 1000;
+    public Account balance;
+    private int boardPosition;
+
+    public Player() {
+        boardPosition = 0;
+        balance = new Account(startBalance);
+    }
+
+    public int getBoardPosition() {
+        return boardPosition;
+    }
+
+    public void moveForward(int spaces) {}
 }
