48afc4427f7d984054ae5ce80b951018f8708d32
Joakim Levorsen <joakim.levorsen@icloud.com>
2018-11-02 13:24:03 +0100

    DiceRoll implementeret

--- src/main/java/DiceRoll.java
+++ src/main/java/DiceRoll.java
@@ -1,2 +1,28 @@
 public class DiceRoll {
+    int rolla, rollb;
+
+    //  Denne constructor definerer terningernes start værdier
+    public DiceRoll(int rollA, int rollB) {
+        rolla = rollA;
+        rollb = rollB;
+    }
+
+    public boolean equals(DiceRoll other) {
+        return other.getRolla() == rolla && other.getRollb() == rollb;
+    }
+
+    //  Returnerer den samlede værdi af spillerens to terningekast
+    public int getValue() {
+        return rolla + rollb;
+    }
+
+    //  Getters
+
+    public int getRolla() {
+        return rolla;
+    }
+
+    public int getRollb() {
+        return rollb;
+    }
 }
